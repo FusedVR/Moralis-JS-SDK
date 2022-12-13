0829c4c6f9ae5af0eff7e493a721fa52b7ea8aaf
Bart Tadych <b4rtaz@gmail.com>
2022-12-13 17:16:30 +0100

    Fused Updates

--- demos/parse-server-migration/src/cloud/cloud.ts
+++ demos/parse-server-migration/src/cloud/cloud.ts
@@ -1,6 +1,6 @@
 declare const Parse: any;
 
-Parse.Cloud.job("Clean Player App Codes older than 10 minutes", async function (request) {
+Parse.Cloud.job("Clean Player App Codes older than 10 minutes", async function (request : any) {
   // params: passed in the job call
   // headers: from the request that triggered the job
   // log: the Moralis Server logger passed in the request
@@ -10,8 +10,8 @@ Parse.Cloud.job("Clean Player App Codes older than 10 minutes", async function (
   var currentDate = new Date();
   var tenMinsAgo = new Date(currentDate.getTime() - 10*60000); //10 mins ago
 
-  var PlayerAppDefinition = Moralis.Object.extend("PlayerApp");
-  let playerAppQuery = new Moralis.Query(PlayerAppDefinition);
+  var PlayerAppDefinition = Parse.Object.extend("PlayerApp");
+  let playerAppQuery = new Parse.Query(PlayerAppDefinition);
   playerAppQuery.lessThan("createdAt", tenMinsAgo);
   var playerApps = await playerAppQuery.find({useMasterKey : true});
 
--- demos/parse-server-migration/src/cloud/job.ts
+++ demos/parse-server-migration/src/cloud/job.ts
@@ -1,4 +1,5 @@
 declare const Parse: any;
+const logger = require('parse-server').logger;
 
 import Scheduler from 'parse-server-jobs-scheduler';
 const scheduler = new Scheduler();
@@ -6,7 +7,7 @@ const scheduler = new Scheduler();
 // Recreates all crons when the server is launched
 scheduler.recreateScheduleForAllJobs();
 
-//https://raw.githubusercontent.com/CaskProtocol/cask-js-sdk/main/src/core/abi/testnet/ICaskSubscriptions.js
+//https://github.com/CaskProtocol/cask-js-sdk/blob/main/src/core/abi/ICaskSubscriptions.js
 //https://docs.cask.fi/protocol-deployments/testnet
 //https://github.com/CaskProtocol/cask-webhook-bridge#webhook-event-list
 //To update this code, can pull the keys from the Moralis website
@@ -14,26 +15,23 @@ scheduler.recreateScheduleForAllJobs();
 // need to setup a flow for renewals but now we can get a subscription and then allow users to associate with an application
 // from the front end we can have one click to subscribe per app, but backend should keep it seperate
 
-Parse.Cloud.afterSave("CaskSubCreate", async function (request : any) {
-  scheduler.recreateSchedule(request.object.id);
-
-  let logger = Moralis.Cloud.getLogger();
+Parse.Cloud.afterSave("CaskSubCreateLogs", async function (request : any) {
   let confirmed = request.object.get("confirmed");
 
   logger.info("Sub Create is Confirmed : " + confirmed);
   logger.info("Plan Id " + request.object.get("planId"));
 
   if (confirmed){
-    var PlansDefinition = Moralis.Object.extend("PlanLimits");
-    let planQuery = new Moralis.Query(PlansDefinition);
+    var PlansDefinition = Parse.Object.extend("PlanLimits");
+    let planQuery = new Parse.Query(PlansDefinition);
     planQuery.equalTo("planId", request.object.get("planId"));
     let plan = await planQuery.first();
 
     if (plan) { //could also check if the provider is me if needed
       logger.info("Subscription Id " + request.object.get("subscriptionId"));
       var attributes = { subscriptionId : request.object.get("subscriptionId"), "planId" : request.object.get("planId") , 
-      address : request.object.get("consumer").toLowerCase(), subscribed : true };
-      var SubsDefinition = Moralis.Object.extend("Subscriptions");
+        address : request.object.get("consumer").toLowerCase(), subscribed : true };
+      var SubsDefinition = Parse.Object.extend("Subscriptions");
       var subscription = new SubsDefinition(attributes);
       await subscription.save(null, {useMasterKey : true}); //only need to save subscription since it needs to be assigned to app
     } else {
@@ -41,97 +39,64 @@ Parse.Cloud.afterSave("CaskSubCreate", async function (request : any) {
     }
   }
 
+  scheduler.recreateSchedule(request.object.id);
 });
 
 //on SubscriptionRenewed we need to reset analytics for the app
-Parse.Cloud.afterSave("CaskSubRenewal", async function (request : any) {
-  scheduler.recreateSchedule(request.object.id);
-
-  let logger = Moralis.Cloud.getLogger();
+Parse.Cloud.afterSave("CaskSubRenewalLogs", async function (request : any) {
   let confirmed = request.object.get("confirmed");
   logger.info("Sub Renewal is Confirmed : " + confirmed);
   if (confirmed) {
-    var SubDefinition = Moralis.Object.extend("Subscriptions");
-    let subQuery = new Moralis.Query(SubDefinition);
+    var SubDefinition = Parse.Object.extend("Subscriptions");
+    let subQuery = new Parse.Query(SubDefinition);
     subQuery.equalTo("subscriptionId", request.object.get("subscriptionId"));
     logger.info("Subscription ID : " +  request.object.get("subscriptionId"));
     let sub = await subQuery.first();
 
-    //TODO consider giving a master key to these Moralis Cloud Functions to call the FusedVR APIs and update
     if (sub) { 
-      if (sub.get("appId")) {
-        logger.info("Subscription App ID : " +  sub.get("appId"));
-        let appQuery = new Moralis.Query("Apps");
-        let app = await appQuery.get(sub.get("appId"));
-  
-        var AppStatsDefinition = Moralis.Object.extend("AppStats");
-        let statsQuery = new Moralis.Query(AppStatsDefinition);
-        statsQuery.equalTo("app", app);
-        statsQuery.descending("createdAt");
-        let stat = await statsQuery.first(); //get previous App Stats for app
-
-        var attributes = { "app" : app , "sentMail" : 0, "emailLimit" : stat.get("emailLimit"), 
-          "apiCalls" : 0, "apiLimit" : stat.get("apiLimit") };
-        var appStats = new AppStatsDefinition(attributes);
-        await appStats.save(null, {useMasterKey : true});
-      }
-
+      await subscription.save({subscribed : true}, {useMasterKey : true}); //update will trigger Subscription Job
     } else {
       logger.info("No associated subscription in Database for Renewal");
     }
   }
+
+  scheduler.recreateSchedule(request.object.id);
 });
 
 //cask sub cancel
-Parse.Cloud.afterSave("CaskSubBurn", async function (request : any) {
-  scheduler.recreateSchedule(request.object.id);
-
-  let logger = Moralis.Cloud.getLogger();
+Parse.Cloud.afterSave("CaskSubCanceledLogs", async function (request : any) {
   let confirmed = request.object.get("confirmed");
   logger.info("Sub Cancel is Confirmed : " + confirmed);
   if (confirmed) {
-    var SubDefinition = Moralis.Object.extend("Subscriptions");
-    let subQuery = new Moralis.Query(SubDefinition);
+    var SubDefinition = Parse.Object.extend("Subscriptions");
+    let subQuery = new Parse.Query(SubDefinition);
     subQuery.equalTo("subscriptionId", request.object.get("subscriptionId"));
     let sub = await subQuery.first();
 
     logger.info( "Cancel Subscription : " + request.object.get("subscriptionId") );
 
-    //TODO consider giving a master key to these Moralis Cloud Functions to call the FusedVR APIs and update
     if (sub) { 
-
-      var PlansDefinition = Moralis.Object.extend("PlanLimits");
-      let planQuery = new Moralis.Query(PlansDefinition);
-      planQuery.equalTo("planId", request.object.get("planId"));
-      let plan = await planQuery.first();
-
-      logger.info( "Removing Subscription from App : " + sub.get("appId") );
-      if (sub.get("appId")) {
-        let appQuery = new Moralis.Query("Apps");
-        let app = await appQuery.get(sub.get("appId"));
-  
-        var AppStatsDefinition = Moralis.Object.extend("AppStats");
-        let statsQuery = new Moralis.Query(AppStatsDefinition);
-        statsQuery.equalTo("app", app);
-        statsQuery.descending("createdAt");
-        let stat = await statsQuery.first(); //get previous App Stats for app
-  
-        let emailLimit = stat.get("emailLimit") - plan.get("emailLimit");
-        let apiLimit = stat.get("apiLimit") - plan.get("apiLimit");
-
-        logger.info( "New Canceled Email Limit : " + emailLimit );
-        logger.info( "New Canceled API Limit : " + apiLimit );
-
-        var attributes = { "app" : app , "sentMail" : 0, "emailLimit" : emailLimit, 
-          "apiCalls" : 0, "apiLimit" : apiLimit };
-        var appStats = new AppStatsDefinition(attributes);
-        await appStats.save(null, {useMasterKey : true});
-      }
-
-      await sub.destroy(); //eventually save the data with a subscribed flag
-      //await sub.save({subscribed : false}, {useMasterKey : true});
+      await sub.save({subscribed : false}, {useMasterKey : true});
     } else {
       logger.info("No assocaited subscription for Sub Cancel");
     }
   }
+
+  scheduler.recreateSchedule(request.object.id);
+});
+
+Parse.Cloud.afterSave("Subscriptions", async function (request : any) {
+  var subID = "";
+  if (request.object.get("subscribed")) {
+    subID = request.object.get("subscriptionId");
+  } else {
+    subID = "free"; //canceled so revert to the free tier. must have sub ID with free as keyword
+  }
+
+  var attributes = { subscriptionId : subID, apiCalls : 0 , emailCalls : 0 };
+  var SubStatsDefinition = Parse.Object.extend("SubStats");
+  var subStats = new SubStatsDefinition(attributes);
+  await subStats.save(null, {useMasterKey : true});
+
+  scheduler.recreateSchedule(request.object.id);
 });
